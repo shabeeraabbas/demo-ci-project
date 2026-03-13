a01f84d1d80a0fc71120fdaed5b3c0550bc4f342
Shabeera Abbas <shabeeraabbas@gmail.com>
2026-03-14 01:30:36 +0530

    chrome update

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,15 +1,17 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 import time
 
 def test_login():
+    options = Options()
+    options.binary_location = "C:\\Program Files\\Google\Chrome\\Application\\chrome.exe"
+    jm sj
+    service = Service("C:\\tools\\chromedriver\\chromedriver.exe")
+    driver = webdriver.Chrome(service=service, options=options)
 
-    driver = webdriver.Chrome()
     driver.get("file:///C:/Users/user/Desktop/demo-ci-project/index.html")
 
     driver.find_element(By.ID,"username").send_keys("testuser")
